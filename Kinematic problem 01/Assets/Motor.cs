d976077ebb825697735af383b6337858847cedbf
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2016-11-23 18:53:49 +0200

    Added project 01

---
+++ Kinematic problem 01/Assets/Motor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Motor : MonoBehaviour {
+
+	public float initialVelocity;
+	public float acceleration;
+	float currentVelocity;
+
+	// Use this for initialization
+	void Start () {
+		currentVelocity = initialVelocity;
+	}
+	
+	void FixedUpdate() {
+		if (Time.fixedTime < Timer.predictedTime) {
+			currentVelocity += acceleration * Time.fixedDeltaTime;
+			transform.Translate (Vector3.right * currentVelocity * Time.fixedDeltaTime);
+		}
+	}
+}
---
+++ Kinematic problem 01/Assets/Timer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public static float predictedTime;
+	public Motor objectA;
+	public Motor objectB;
+
+	public float timeStep = 0.02f;
+
+	// Use this for initialization
+	void Start () {
+
+		Time.fixedDeltaTime = timeStep;
+
+		float h = objectA.transform.position.x - objectB.transform.position.x;
+
+		float a = objectB.acceleration - objectA.acceleration;
+		float b = 2 * (objectB.initialVelocity - objectA.initialVelocity);
+		float c = -2 * h;
+
+		predictedTime = (-b + Mathf.Sqrt (b * b - 4 * a * c)) / (2 * a);
+		print (predictedTime);
+	}
+
+}
